95a9f88143e135e83bb31007c792855e04e2f20c
Ben Wingerter <benwingerter01@gmail.com>
2017-03-29 18:21:53 -0500

    additional methods

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,3 +1,5 @@
+/* jshint node: true */
+
 "use strict";
 
 const socketio = require("socket.io"),
@@ -11,6 +13,8 @@ const socketio = require("socket.io"),
 
 module.exports.listen = function(server) {
 
+	let locations = [];
+
 	let io = socketio.listen(server);
 
 	io.use(socketioJwt.authorize({
@@ -22,9 +26,79 @@ module.exports.listen = function(server) {
 
 		socket.on("shareLocation", (data) => {
 
-			socket.broadcast.emit("mapLocation", {id: socket.decoded_token.userId, lat: data.lat, lng: data.lng});
+			knex.select("firstName, lastName, picture").from("users").where("id", "=", socket.decoded_token.userId).then((rows) => {
+
+				let index;
+
+				for(let i in locations) {
+
+					if(locations[i].id === socket.decoded_token.userId) {
+
+						index = i;
+
+					} else {
+
+						index = undefined;
+
+					}
+
+				}
+
+				const userData = {
+					id: socket.decoded_token.userId,
+					name: rows[0].firstName + rows[0].lastName,
+					picture: rows[0].picture,
+					lat: data.lat,
+					lng: data.lng,
+					time: Date.now()
+				};
+
+				if(index === undefined) {
+
+					locations.push(userData);
+
+				} else {
+
+					locations[index] = userData;
+
+				}
+
+				socket.broadcast.emit("mapLocation", {id: socket.decoded_token.userId, lat: data.lat, lng: data.lng});
+
+			});
 
 		});
+
+		socket.on("getAllLocations", (data) => {
+
+			socket.emit(locations);
+
+		});
+
+		socket.on("stopSharingLocation", (data) => {
+
+			socket.broadcast.emit("removeLocation", {id: socket.decoded_token.userId});
+
+		});
+
+	});
+
+	setInterval(() => {
+
+		const currentTime = Date.now();
+
+		for(let i = locations.length -1; i >= 0; i--) { //start from back of array to prevent array index of becoming corrupt during loop
+
+			if(locations[i].time - 15000 < currentTime) {
+
+				locations.splice(i, 1);
+
+				io.sockets.emit("removeLocation", {id: locations[i].id});
+				//@todo keep in mind while building client that this sends to ALL clients;
+
+			}
+
+		}
 
 	});
 
