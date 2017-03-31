937986c381d93954211be3c42a39fdc15d2beab6
Ben Wingerter <benwingerter01@gmail.com>
2017-03-31 10:39:01 -0500

    fix time comparison

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -89,7 +89,7 @@ module.exports.listen = function(server) {
 
 		for(let i = locations.length -1; i >= 0; i--) { //start from back of array to prevent array index of becoming corrupt during loop
 
-			if(locations[i].time - 15000 < currentTime) {
+			if(locations[i].time + 15000 < currentTime) {
 
 				io.sockets.emit("removeLocation", {id: locations[i].id});
 				//@todo keep in mind while building client that this sends to ALL clients;
