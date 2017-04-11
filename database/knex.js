478fa038c39680ca112a1b34769565a6a89f7e3f
Ben Wingerter <benwingerter01@gmail.com>
2017-04-11 17:55:29 -0500

    database update

--- database/knex.js
+++ database/knex.js
@@ -7,7 +7,17 @@ if(process.env.NODE_ENV === "production") {
 
 	knex = require("knex")({
 		client: "pg",
-		connection: "postgres://nlghwvfayvoifk:af3da8521ddd4a222cf85f36c84f68af569e169387479ed40fff6dd15dd82d91@ec2-107-20-163-238.compute-1.amazonaws.com:5432/dfic3203som2bm"
+		//connection: "postgres://airmeet:!beliEVEthAtwEW!LLuuin9876@airmeet.cnanojewxddp.us-east-2.rds.amazonaws.com:5432/airmeet",
+		connection: {
+			host: 'airmeet.cnanojewxddp.us-east-2.rds.amazonaws.com',
+			user: 'airmeet',
+			password: '!beliEVEthAtwEW!LLuuin9876',
+			database: 'airmeet'
+		},
+		pool: {
+			min: 2,
+			max: 50
+		}
 	});
 
 } else {
